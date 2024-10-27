0e4c3030f0c4b7e0568332eedda3974474185873
Pranav.C <pranavparathakkad@gmail.com>
2024-10-27 10:28:49 +0530

    Third Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,18 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'tailwindPr1';
+// }
+
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,8 +21,13 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'tailwindPr1';
+  isNavbarOpen = false; // State for navbar toggle
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen; // Toggle navbar state
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
 
 export const routes: Routes = [{path:'about' , component:AboutComponent},
-    {path:'profile' , component:ProfileComponent}
+    {path:'profile' , component:ProfileComponent},
+    {path:'skills' , component:SkillsComponent}
 ];
